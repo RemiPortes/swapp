bcb736cc752e57ceedeea743d10de127aa25349b
Tweelia <clelia.bondu@gmail.com>
2014-11-28 09:46:15 +0100

    scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :matches
+
+  resources :likes
+
+  resources :users
+
   resources :items
 
   # The priority is based upon order of creation: first created -> highest priority.
